cfb89ce3dd50b4fc97abbf8d36c13310aca2ef34
Ben Polgardy <benpolgardy@gmail.com>
2019-10-18 11:56:04 +0200

    add event listeners, add event handling, add handlePlayerTurn(), add fieldIsTaken()

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,58 @@
-alert('You should write code into "static/js/game.js" to make it work');
+function handleClick(event) {
+    handlePlayerTurn();
+} 
+
+
+function addEventListenerToCells() {
+    const cells = document.querySelectorAll('.game-cell');
+    
+    for (cell of cells) {
+        cell.addEventListener('click', handleClick);
+    };
+};
+
+
+function handlePlayerTurn() {
+    
+    if (playerOneTurn) {
+        if (!fieldIsTaken()) {
+            event.target.innerText = "x";
+            playerOneTurn = false;
+        };
+    } else {
+        if (!fieldIsTaken()) {
+            event.target.innerText = "o";
+            playerOneTurn = true;
+        };
+    };
+};
+
+
+function fieldIsTaken() {
+    if (event.target.innerText) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+
+function game() {
+    addEventListenerToCells();
+     
+    let playerWon = false;
+    
+    /*while (!playerWon) {
+        continue;
+    };*/
+    
+}
+
+addEventListenerToCells();
+
+let playerOneTurn = true; 
+
+/*const cellYCoordinate = event.target.dataset.coordinateY;
+const cellXCoordinate = event.target.dataset.coordinateX;
+event.target.innerText = "x";
+switchPlayer();*/
